25e8a45c548c5460712ccc726c5e6a70803551fb
marcos pinardi <exegeses@gmail.com>
2020-07-13 15:30:56 -0300

    clase Persona

---
+++ clase2/Persona.php
@@ -0,0 +1,35 @@
+<?php
+
+    class Persona
+    {
+        private $nombre;
+        private $apellido;
+
+        public function verDatos()
+        {
+            $mensaje = 'Nombre: '. $this->getNombre();
+            $mensaje .= '<br>';
+            $mensaje .= 'Apellido: '.$this->getApellido();
+            $mensaje .= '<br>';
+            return $mensaje;
+        }
+
+        public function getNombre()
+        {
+            return $this->nombre;
+        }
+        public function setNombre($nombre)
+        {
+            $this->nombre = $nombre;
+        }
+
+        public function getApellido()
+        {
+            return $this->apellido;
+        }
+        public function setApellido($apellido)
+        {
+            $this->apellido = $apellido;
+        }
+
+    }
