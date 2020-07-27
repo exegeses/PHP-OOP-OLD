e33cb4057948c869288d24536b7db7f225da989b
marcos pinardi <exegeses@gmail.com>
2020-07-27 15:27:30 -0300

    formulario de alta de un destino

---
+++ agencia/formAgregarDestino.php
@@ -0,0 +1,48 @@
+<?php
+    require 'config/config.php';
+    include 'includes/over-all-header.html';
+    include 'includes/nav.php';
+?>
+        <main class="container bg-light rounded text-secondary">
+            <h1 class="py-3">Alta de un nuevo destino</h1>
+                <div class="bg-light border p-4 m-4" >
+                    <form action="agregarDestino.php" method="post">
+                        Destino:
+                        <br>
+                        <input type="text" name="destNombre" class="form-control" required>
+                        <br>
+                        Región:
+                        <br>
+                        <select name="regID" class="form-control" required>
+                            <option value="">Seleccione una región</option>
+
+                            <option value="1">nombre región</option>
+
+                        </select>
+                        <br>
+                        Precio:
+                        <br>
+                        <input type="number" name="destPrecio" class="form-control"
+                               step="0.01" min="0" required>
+                        <br>
+                        Asientos Totales:
+                        <br>
+                        <input type="number" name="destAsientos" class="form-control"
+                               step="1" min="0" required>
+                        <br>
+                        Asientos Disponibles:
+                        <br>
+                        <input type="number" name="destDisponibles" class="form-control"
+                               step="1" min="0" required>
+                        <br>
+                        <button class="btn btn-dark">Agregar Destino</button>
+                        <a href="adminRegiones.php" class="btn btn-outline-secondary">
+                            Volver al panel de regiones
+                        </a>
+                    </form>
+                </div>
+        </main>
+
+<?php
+include 'includes/footer.php';
+?>
--- agencia/formAgregarRegion.php
+++ agencia/formAgregarRegion.php
@@ -6,7 +6,7 @@
         <main class="container bg-light rounded text-secondary">
             <h1 class="py-3">Alta de una nueva región</h1>
                 <div class="bg-light border p-4 m-4" >
-                    <form action="agregarRegion.php" method="post" >
+                    <form action="agregarRegion.php" method="post">
                         Región:
                         <br>
                         <input type="text" name="regNombre" class="form-control" required>
